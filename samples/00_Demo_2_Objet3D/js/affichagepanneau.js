483fa0ceac294681e7fd230d059673d88a78f81b
AlexysDaSilvaOliveira <alexys.dasilvaoliveira@iut-rodez.fr>
2022-06-02 14:09:22 +0200

    ajout commentaire dans le JS fonction 3D

--- samples/00_Demo_2_Objet3D/js/affichagepanneau.js
+++ samples/00_Demo_2_Objet3D/js/affichagepanneau.js
@@ -6,15 +6,25 @@ var World = {
         World.createCones();
         World.createTracker();
     },
-
+		
+	/*
+		Créer le modèle et l'ajoute à la liste des drawables
+	*/
     createCones: function createConesFn() {
 
         var cone = World.getCone();
         World.drawables.push(cone);
     },
-
+	
+	/*
+		Créer l'augmentation correspondant au modèle
+	*/
     getCone: function getConeFn() {
-
+		
+		/*
+            On dit à l'application de charger un modèle 3D
+			on indique la taille du modèle et sa position
+        */
         return new AR.Model("assets/panneau.wt3", {
             scale: {
                 x: 1,
@@ -32,7 +42,10 @@ var World = {
             onError: World.onError
         });
     },
-
+	
+	/*
+		Mise en place du tracker : le fichier .wto créé avec Wikitude Studio
+    */
     createTracker: function createTrackerFn() {
         this.targetCollectionResource = new AR.TargetCollectionResource("assets/extincteur.wto", {
             onError: World.onError
@@ -41,7 +54,11 @@ var World = {
         this.tracker = new AR.ObjectTracker(this.targetCollectionResource, {
             onError: World.onError
         });
-
+		
+		/*
+            Ici, on créé une variable objectTrackable qui permet à l'application de savoir 
+			qu'elle doit tracker cet objet.
+         */
         this.objectTrackable = new AR.ObjectTrackable(this.tracker, "*", {
             drawables: {
                 cam: World.drawables
@@ -51,26 +68,41 @@ var World = {
             onError: World.onError
         });
     },
-
+	
+	/*
+		Fonction qui est lancée lors de la reconnaissance de l'objet
+		Appel une fonction qui affiche tous les drawables
+	*/
     objectRecognized: function objectRecognizedFn() {
         World.hideInfoBar();
         World.setAugmentationsEnabled(true);
     },
-
+	
+	/*
+		Fonction qui est lancée lors de la perte de reconnaissance de l'objet
+		Appel une fonction qui cache tous les drawables
+	*/
     objectLost: function objectLostFn() {
         World.setAugmentationsEnabled(false);
     },
-
+	
+	/*
+		Affiche ou cache les drawables selon la valeur du
+		paramètre 'enable'
+	*/
     setAugmentationsEnabled: function setAugmentationsEnabledFn(enabled) {
         for (var i = 0; i < World.drawables.length; i++) {
             World.drawables[i].enabled = enabled;
         }
     },
-
+	
+	/*
+		Erreur lors du chargement de l'Architecture World
+	*/
     onError: function onErrorFn(error) {
         alert(error);
     },
-
+	
     hideInfoBar: function hideInfoBarFn() {
         document.getElementById("infoBox").style.display = "none";
     },
